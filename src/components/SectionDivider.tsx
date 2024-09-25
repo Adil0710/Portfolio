56102dbd927bb2425246db6c4ee3b48d297a273c
unknown <padil2246@gmail.com>
2024-09-26 00:02:56 +0530

    adjusted margin about

--- src/components/SectionDivider.tsx
+++ src/components/SectionDivider.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function SectionDivider() {
   return (
-    <div className=' dark:bg-gray-400 bg-gray-600 my-24 mt-5  h-16 w-1 rounded-full hidden sm:block'></div>
+    <div className=' dark:bg-gray-400 bg-gray-600 mb-28 mt-0 z-50 h-16 w-1 rounded-full hidden sm:block'></div>
   )
 }
 

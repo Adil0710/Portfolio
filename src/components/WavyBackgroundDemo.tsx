c077a6a506095f101acef7fe0ed1b060d34c972a
unknown <padil2246@gmail.com>
2024-10-04 00:59:25 +0530

    added loading

--- src/components/WavyBackgroundDemo.tsx
+++ src/components/WavyBackgroundDemo.tsx
@@ -1,17 +1,47 @@
 "use client";
 import React from "react";
 import { WavyBackground } from "./ui/wavy-background";
+import IconCloud from "@/components/ui/icon-cloud";
 
+const slugs = [
+  "typescript",
+  "javascript",
+  "dart",
+  "java",
+  "react",
+  "flutter",
+  "android",
+  "html5",
+  "css3",
+  "nodedotjs",
+  "express",
+  "nextdotjs",
+  "prisma",
+  "amazonaws",
+  "postgresql",
+  "firebase",
+  "nginx",
+  "vercel",
+  "testinglibrary",
+  "jest",
+  "cypress",
+  "docker",
+  "git",
+  "jira",
+  "github",
+  "gitlab",
+  "visualstudiocode",
+  "androidstudio",
+  "sonarqube",
+  "figma",
+];
 
 export function WavyBackgroundDemo() {
   return (
     <WavyBackground className="max-w-4xl mx-auto pb-40 text-black dark:text-white">
-      <p className="text-2xl md:text-4xl lg:text-7xl  font-bold inter-var text-center">
-        Hero waves are cool
-      </p>
-      <p className="text-base md:text-lg mt-4  font-normal inter-var text-center">
-        Leverage the power of canvas to create a beautiful hero section
-      </p>
+      <div className="relative flex size-full max-w-lg items-center justify-center overflow-hidden rounded-lg px-20 pb-20 pt-8 ">
+        <IconCloud iconSlugs={slugs} />
+      </div>
     </WavyBackground>
   );
 }
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,18 +1,6 @@
-import { ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+  return twMerge(clsx(inputs))
 }
-
-export const validateString = (
-  value: unknown,
-  maxLength: number
-): value is string => {
-  if (!value || typeof value !== "string" || value.length > maxLength) {
-    return false;
-  }
-
-  return true;
-};
-

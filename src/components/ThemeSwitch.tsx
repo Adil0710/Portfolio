622332ae18dfd8f48fca4b787026afa2b68b7851
Adil0710 <padil2246@gmail.com>
2024-11-21 12:05:51 +0530

    done

--- src/components/ThemeSwitch.tsx
+++ src/components/ThemeSwitch.tsx
@@ -14,9 +14,9 @@ const ThemeSwitch = () => {
     >
       <span>
         {theme === "light" ? (
-          <SunIcon className="h-[1.1rem] w-[1.1rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <SunIcon className="h-[1.1rem] w-[1.1rem] rotate-0 scale-100 dark:-rotate-90 dark:scale-0" />
         ) : (
-          <MoonIcon className="h-[1.1rem] w-[1.1rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <MoonIcon className="h-[1.1rem] w-[1.1rem] rotate-90 scale-0 dark:rotate-0 dark:scale-100" />
         )}
       </span>
       <span className="absolute inset-x-0 w-1/2 mx-auto -bottom-px bg-gradient-to-r from-transparent via-emerald-500 to-transparent h-px" />

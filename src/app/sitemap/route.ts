d0ed973d69121158d7d82a0f83ce184c7d18902a
Adil0710 <padil2246@gmail.com>
2024-12-11 12:08:02 +0530

    added tech

--- src/app/sitemap/route.ts
+++ src/app/sitemap/route.ts
@@ -1,16 +1,16 @@
-export default function Sitemap() {
+export async function GET() {
     const baseUrl = "https://devadil.vercel.app"; // Replace with your actual domain
   
     return new Response(
       `
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    <url>
-      <loc>${baseUrl}/</loc>
-      <lastmod>${new Date().toISOString()}</lastmod>
-      <changefreq>monthly</changefreq>
-      <priority>1.0</priority>
-    </url>
-  </urlset>
+      <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+        <url>
+          <loc>${baseUrl}/</loc>
+          <lastmod>${new Date().toISOString()}</lastmod>
+          <changefreq>monthly</changefreq>
+          <priority>1.0</priority>
+        </url>
+      </urlset>
       `,
       {
         headers: {

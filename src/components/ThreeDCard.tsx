70ab3c28ac4d3e35900fcec4cd8b808bb79ec623
Adil0710 <padil2246@gmail.com>
2024-12-11 17:22:50 +0530

    style

--- src/components/ThreeDCard.tsx
+++ src/components/ThreeDCard.tsx
@@ -33,7 +33,7 @@ export function ThreeDCardDemo() {
                 {project.description}
               </CardItem>
 
-              <div className="sm:mt-1 mt-3 flex flex-row flex-wrap gap-2">
+              <div className="sm:mt-1 mt-1.5 flex flex-row flex-wrap gap-2">
                 {project.tech.map((techno, index) => (
                   <CardItem
                     key={index}
